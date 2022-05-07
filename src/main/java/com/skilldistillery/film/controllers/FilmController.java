fd9b741f804ae768b25b5d51fe8f6fc6f21b93bc
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-05-06 20:23:58 -0600

    Nates commit controller update

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,20 +1,57 @@
 package com.skilldistillery.film.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.dao.FilmDAO;
+import com.skilldistillery.film.entities.Film;
+import com.skilldistillery.president.data.President;
 
 @Controller
 public class FilmController {
 
 	@Autowired
-	private FilmDAO filmDAO;
+	private FilmDAO dao;
 	
 	
 	@RequestMapping(path = {"/", "home.do"})
 	public String home() {
+		
+		//needs path
+		//needs params
+		//needs method
+		
+		//needs model and view
+		
+//		mv.setViewName("WEB-INF/film.jsp");
+//		mv.addObject("presidents", presidents);
+//		return mv;
+
+//inputs jsps
+		
+//output, single page
+		
+		
+		@RequestMapping(path = "filmInfo.do", params = "id", method = RequestMethod.GET)
+		public ModelAndView getFilmById(@RequestParam("id") String party) {
+			
+			Film film = dao.getFilmById(party);
+			
+			ModelAndView mv = new ModelAndView();
+			mv.setViewName("WEB-INF/presidents.jsp");
+			mv.addObject("presidents", presidents);
+			
+			return mv;
+		}
+		
+		
+		
 		
 		return "WEB-INF/views/home.jsp";
 	}
--- src/main/java/com/skilldistillery/film/dao/FilmDaoJdbcImpl.java
+++ src/main/java/com/skilldistillery/film/dao/FilmDaoJdbcImpl.java
@@ -1,8 +1,127 @@
 package com.skilldistillery.film.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
 
 @Component
 public class FilmDaoJdbcImpl implements FilmDAO {
+
+	private static final String url = "jdbc:mysql://localhost:3306/sdvid?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=US/Mountain";
+	private static final String driver = "com.mysql.cj.jdbc.Driver";
+	private static final String user = "student";
+	private static final String pass = "student";
+	
+	static {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String getUrl() {
+		return url;
+	}
+
+	public static String getDriver() {
+		return driver;
+	}
+
+	public static String getUser() {
+		return user;
+	}
+
+	public static String getPass() {
+		return pass;
+	}
+
+	@Override
+	public Film findFilmById(int filmId) {
+		Film film = null;
+		try {
+			Connection conn = DriverManager.getConnection(FilmDaoJdbcImpl.getUrl(),
+					FilmDaoJdbcImpl.getUser(), FilmDaoJdbcImpl.getPass());
+			String sql = "SELECT film.id, title, description, release_year, language_id, language.name, rental_duration, ";
+				  sql += " rental_rate, length, replacement_cost, rating, special_features "
+					   + " FROM film"
+					   + " JOIN film_actor ON film.id = film_actor.film_id "
+					   + " JOIN language ON film.language_id = language.id "
+					   + " WHERE actor_id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, filmId);
+			ResultSet filmResult = stmt.executeQuery();
+			if (filmResult.next()) {
+				film = new Film(); // Create the object
+				// Here is our mapping of query columns to our object fields:
+
+				film.setId(filmResult.getInt("id"));
+				film.setTitle(filmResult.getString("title"));
+				film.setDescription(filmResult.getString("description"));
+				film.setReleaseYear(filmResult.getShort("release_year"));
+				film.setLanguageId(filmResult.getInt("language_id"));
+				film.setLanguage(filmResult.getString("name"));
+				film.setRentalDuration(filmResult.getInt("rental_duration"));
+				film.setRate(filmResult.getDouble("rental_rate"));
+				film.setLength(filmResult.getInt("length"));
+				film.setReplacementCost(filmResult.getDouble("replacement_cost"));
+				film.setRating(filmResult.getString("rating"));
+				film.setFeatures(filmResult.getString("special_features"));
+				film.setCast(findActorsByFilmId(filmId));
+			}
+
+			filmResult.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return film;
+
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findCategoryByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Film> findFilmsByKeyword(String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Film createFilm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteFilm() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editFilm() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
